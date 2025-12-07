6cefdc0dcb41bfce1d69015012f7d503ed6ba5d7
Gfrim <gfrim79@gmail.com>
2025-12-07 16:31:01 +0000

    n/a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,17 @@ import localFont from "next/font/local";
 const generalSans = localFont({
   src: [
     {
-      path: "fonts/GeneralSans-Regular.woff2",
+      path: "public/fonts/GeneralSans-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "fonts/GeneralSans-Medium.woff2",
+      path: "public/fonts/GeneralSans-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "fonts/GeneralSans-Semibold.woff2",
+      path: "public/fonts/GeneralSans-Semibold.woff2",
       weight: "600",
       style: "normal",
     },
