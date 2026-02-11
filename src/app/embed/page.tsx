fea0eb9976c11b78af663bdb9e54a38eab88ad8e
Gfrim <84197818+Gfrim@users.noreply.github.com>
2026-02-11 21:14:45 +0000

    Change background color of embed page to transparent

--- src/app/embed/page.tsx
+++ src/app/embed/page.tsx
@@ -4,7 +4,7 @@ import { ChatProvider } from '@/components/chat-provider';
 export default function EmbedPage() {
   // This page provides the chat widget in a clean, embeddable format.
   return (
-    <div className="w-full h-screen">
+    <div className="w-full bg-transparent">
       <ChatProvider />
     </div>
   );
